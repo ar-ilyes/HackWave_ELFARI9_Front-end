3892115f2b6cf8781eeb3e888940713cbed0119f
ilyes <armilyes@gmail.com>
2023-09-22 01:32:53 +0200

    adding hero page and login , register pages

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import logo from '../assets/logo.png';
 const Navbar =()=>{
-    return (<div className='w-full blueColor flex flex-row items-center justify-between'>
-    <Image src={logo} className='mx-2'></Image>
-    <div className='flex justify-center items-center space-x-6 mx-4 py-2'>
-    <button className='yellowTextColor text-lg border-none'>contact</button>
-    <button className='yellowTextColor text-lg border-none'>help</button>
-    <button className='yellowTextColor text-lg border-solid rounded-lg yellowBorderColor border-2 py-1 px-3 hover:scale-110 transition-all duration-500 hover:blueTextColor hover:yellowColor'>register</button>
-    <button className='yellowTextColor text-lg border-solid rounded-lg yellowBorderColor border-2 py-1 px-3 hover:scale-110 transition-all duration-500  hover:blueTextColor hover:yellowColor'>sign up</button>
+    return (<div className='w-full blueColor flex flex-row items-center justify-between absolute'>
+    <Image src={logo} className='mx-5'></Image>
+    <div className='flex justify-center items-center space-x-6 mx-4 py-2 '>
+    <button className='yellowTextColor text-lg border-none hover:scale-110 transition-all duration-500'>contact</button>
+    <button className='yellowTextColor text-lg border-none hover:scale-110 transition-all duration-500'>help</button>
+    <button className=' text-lg border-solid rounded-lg  border-2 py-1 px-3 hover:scale-110 transition-all duration-500 switchColor'>register</button>
+    <button className=' text-lg border-solid rounded-lg  border-2 py-1 px-3 hover:scale-110 transition-all duration-500 switchColor'>sign up</button>
     </div>
 </div>);
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
 import Navbar from './components/Navbar';
+import Image from 'next/image';
+import HeroImage from "./assets/heroImage.png"; 
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="w-full h-full backgroundColor">
       <Navbar/>
+      <div className=' container flex flex-row items-center justify-between h-screen '>
+        <div className=' h-full pl-11 flex flex-col justify-center space-y-7 blueTextColor'>
+          <h1 className=' text-4xl font-extrabold '>Whether you are a <span className='yellowTextColor font-black'> Student, Teacher, or Parent</span></h1>
+          <p className=' font-medium text-xl w-3/4'>Join our platform and get ready to embark on a journey of educational excellence and seamless collaboration.</p>
+        </div>
+        <div className=' w-full h-full  flex justify-center items-center'>
+          <Image src={HeroImage} className=' w-3/4 mt-20 '></Image>
+        </div>
+      </div>
     </main>
   )
 }
